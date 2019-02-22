ea1f3cb49bb91f93209168d2e20d4964864bf2a1
C.Rodriguez <crod@madebycrod.com>
2019-02-22 13:22:28 -0700

    update home and about and add products page

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['submit'])){
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$name = $_POST['firstname']." ". $_POST['lastname'];
+$email = $_POST['email'];
+$interest = $_POST['interest'];
+$subject = "Contact form Entry: ".$interest;
+$tel = $_POST['tel'];
+$message = $_POST['message'];
+$mailTo = "me@cliffrodriguez.com";
+$headers = "From:".$email;
+$txt = "You have received and email from " .$firstname.".".$lastname.".\n\n".$tel.".\n\n".$interest."\n\n".$message."\n\n".$name;
+mail($mailTo, $subject, $txt, $headers);
+header( 'Location: https://www.wackohemp.com/thankyou.html' ) ;
+exit;
+
+}
+
+
+
